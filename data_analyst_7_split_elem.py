e496d3e09cae513d08592b523ae721289c12e9f3
Elim Garak <203994+cardassian-tailor@users.noreply.github.com>
2018-12-20 17:07:58 -0500

    Update data_analyst_7_split_elem.py

--- data_analyst_7_split_elem.py
+++ data_analyst_7_split_elem.py
@@ -1,3 +1,6 @@
+# From the data analyst track, section 7 in intro to python. 
+# Splitting elements in a list. Here I used (on line 9) a generator instead of a for loop 
+
 final_data = []
 f = open('crime_rates.csv', 'r')
 data = f.read()
@@ -5,7 +8,9 @@ rows = data.split('\n')
 print(rows[0:5])
 
 [final_data.append(x.split(',')) for x in rows]
+
 '''
+# This also works
 for item in rows:
     final_data.append(item.split(','))
     
