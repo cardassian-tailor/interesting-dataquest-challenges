b8032d67f092bd2f1bd7996d86d555f8d6f0f155
Elim Garak <203994+cardassian-tailor@users.noreply.github.com>
2018-12-21 20:43:40 -0500

    Create challenge-files-loops-cond.py

---
+++ challenge-files-loops-cond.py
@@ -0,0 +1,7 @@
+# step six in challenge files loops and conditions
+# convert numerical values
+
+print(nested_list[0:5])
+
+numerical_list = [[name, float(num)] for name, num in nested_list]
+print(numerical_list)
